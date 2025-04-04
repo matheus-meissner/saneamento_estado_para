f103813ba79f2401fd4871325262bde377c1bd3d
Matheus Meissner <148015612+matheus-meissner@users.noreply.github.com>
2025-04-04 17:50:20 -0300

    fetch: adiciona_belem

--- scripts/processamento_saneamento.py
+++ scripts/processamento_saneamento.py
@@ -1,30 +1,40 @@
 import pandas as pd
 import geopandas as gpd
 from unidecode import unidecode
+import re
 
 # Municípios de interesse
-MUNICIPIOS = ['BARCARENA', 'BELÉM', 'BREVES']
+MUNICIPIOS = ['BARCARENA', 'BELEM', 'BREVES']
 
 # Caminhos dos arquivos
 CAMINHO_SHAPEFILE = 'dados_shapefile/PA_Municipios_2022.shp'
 CAMINHO_SALVAR = 'dados_finais/'
 
+# Função para limpar e padronizar nomes de municípios
+def limpar_nome(mun):
+    mun = unidecode(str(mun))
+    mun = re.sub(r'[^\w\s]', '', mun)
+    mun = mun.strip().upper()
+    if 'BELACM' in mun or ('BEL' in mun and 'CM' in mun):
+        return 'BELEM'
+    return mun
+
 # Função genérica para ler e filtrar dados do SNIS
 def ler_dados_snis(caminho_arquivo, coluna_valor, novo_nome_coluna):
     df = pd.read_csv(caminho_arquivo, sep=';', encoding='latin1')
-    df = df[df['Municipio'].str.upper().isin(MUNICIPIOS)]
+    df['Municipio'] = df['Municipio'].apply(limpar_nome)
+    df = df[df['Municipio'].isin(MUNICIPIOS)]
     df = df[['Municipio', coluna_valor]].rename(columns={
         'Municipio': 'municipio',
         coluna_valor: novo_nome_coluna
     })
-    df['municipio'] = df['municipio'].str.upper()
     return df
 
-# Função para integrar com shapefile
+# Função para integrar dados com shapefile
 def integrar_com_geometria(df_valores, nome_coluna_valor, nome_arquivo_saida):
     gdf = gpd.read_file(CAMINHO_SHAPEFILE)
-    gdf = gdf[gdf['NM_MUN'].str.upper().isin(MUNICIPIOS)]
-    gdf['NM_MUN'] = gdf['NM_MUN'].str.upper()
+    gdf['NM_MUN'] = gdf['NM_MUN'].apply(limpar_nome)
+    gdf = gdf[gdf['NM_MUN'].isin(MUNICIPIOS)]
 
     gdf_merged = gdf.merge(df_valores, left_on='NM_MUN', right_on='municipio')
     caminho_saida = f'{CAMINHO_SALVAR}{nome_arquivo_saida}.geojson'
